60aa8859cb75b69c4a189c5b6980ad128975e1ac
728918 <728918@ELF6IT215814.ACDM.DS.SAIT.CA>
2017-10-26 14:34:22 -0600

    Week 5 lab FINAL

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,41 +14,81 @@ public class ShoppingListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         HttpSession mysession = request.getSession();
-       if (request.getParameter("function") != null){
-            if (request.getParameter("function").equals("logout")){
-            request.setAttribute("message", "Logged Out");
-            
-            mysession.removeAttribute("userObject");
-            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        if (request.getParameter("action") != null) {
+            if (request.getParameter("action").equals("logout")) {
+                request.setAttribute("message", "Logged Out");
+
+                mysession.removeAttribute("userObject");
+                mysession.removeAttribute("array");
+                getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+            }
         }
-        }
-       
-       
-       
-       if (mysession.getAttribute("userObject") != null) {
-           String userName = (String) mysession.getAttribute("userName");
-           request.setAttribute("username", userName);
-                      getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+
+        if (mysession.getAttribute("userObject") != null) {
+            if (mysession.getAttribute("array") == null) {
+                ArrayList<String> listitems = new <String> ArrayList();
+                mysession.setAttribute("array", listitems);
+                request.setAttribute("items", listitems);
+            } else {
+                ArrayList listitems = (ArrayList) mysession.getAttribute("array");
+                request.setAttribute("items", listitems);
+            }
+            String userName = (String) mysession.getAttribute("userObject");
+            request.setAttribute("username", userName);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
         } else {
             getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
-        }  
-       
-       
-       
+        }
+
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        if (request.getParameter("action") != null && request.getParameter("action").equals("register")){
+
+        if (request.getParameter("action") != null && request.getParameter("action").equals("register")) {
             String username = request.getParameter("uname");
             if (username != null && !username.equals("")) {
+                HttpSession mysession = request.getSession();
+                mysession.setAttribute("userObject", username);
                 
+                response.sendRedirect("ShoppingList");
+            } else {
+                request.setAttribute("message", "Please enter a name to use");
+                getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
             }
+        } else if (request.getParameter("action") != null && request.getParameter("action").equals("add")) {
+            String newitem = (String) request.getParameter("additem");
+            if (newitem != null && !newitem.equals("")) {
+
+                HttpSession mysession = request.getSession();
+                ArrayList <String> itemList = (ArrayList<String>) mysession.getAttribute("array");
+                itemList.add(newitem);
+                mysession.setAttribute("array", itemList);
+                
+                response.sendRedirect("ShoppingList");
+            }
+        }
+
+        else if (request.getParameter("action") != null && request.getParameter("action").equals("delete")) {
+            String deletethis = (String) request.getParameter("delete");
+           
+
+                HttpSession mysession = request.getSession();
+                ArrayList <String> itemList = (ArrayList<String>) mysession.getAttribute("array");
+                
+                for (int i = 0; i<itemList.size();i++){
+                if(deletethis.equals(itemList.get(i))){
+                    itemList.remove(i);
+                    i--;
+                }
+                
+                }
+                mysession.setAttribute("array", itemList);
+                
+                response.sendRedirect("ShoppingList");
         }
         
     }
--- web/WEB-INF/shoppingList.jsp
+++ web/WEB-INF/shoppingList.jsp
@@ -14,20 +14,23 @@
     </head>
     <body>
         <h1>Shopping List</h1><br>
-        Hello, ${username} <a href="ShoppingList?function=logout">logout</a>
+        Hello, ${username} <a href="ShoppingList?action=logout">logout</a>
         <br>
-        <h1>List</h1><br>
-        Add item <input type="textfield" name="additemtextfield"> <input type="submit" value="Add"><br>
-        
+        <form action="ShoppingList" method="post">
+            <h1>List</h1><br>
+            Add item <input type="textfield" name="additem"> <input type="hidden" name="action" value="add"> <input type="submit" value="Add"><br>
+        </form>
+        <form action="ShoppingList" method="post">
         <table>
-                <c:forEach items="${items}" var="item">
-                    <tr>
-                         <td><input type="radio" name="delet" value="${item.name}"></td>
-                        <td>${item.name}</td>
-                    </tr>
-                </c:forEach>
-            </table>
-        
-                    <input type="submit" name="deleteUserFN" value="Delete">
+            <c:forEach items="${items}" var="item">
+                <tr>
+                    <td><input type="radio" name="delete" value="${item}"></td>
+                    <td>${item}</td>
+                </tr>
+            </c:forEach>
+        </table>
+            <input type="hidden" name="action" value="delete">
+        <input type="submit" name="deleteUserFN" value="Delete">
+        </form>
     </body>
 </html>
